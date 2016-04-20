72703a420a6a9517c41ec6cc3020045f02232209
Dixit <dixitk13@ccs.neu.edu>
2016-04-20 15:21:49 -0400

    adding master_coord

--- zookeeper/master_coord.c
+++ zookeeper/master_coord.c
@@ -11,72 +11,71 @@ static int connected;
 char gpath[1024];  
 // static char mycontext[] = "testingdata";
 char buffer[255]; 
-int master_number = 999;	
+int master_number = 999;    
 char master_string[23];
 struct String_vector vector; 
 static char master_znode_base_path[] = "/master/dmtcp_coord_";
 
 
-void watcher(zhandle_t *zzh, int type, int state, const char *path,
-             void* context)                                        
-{                                                                  
-    if (type == ZOO_SESSION_EVENT) {   
-    	printf("zoo event changed : watcher \n");                            
-        if (state == ZOO_CONNECTED_STATE) {                        
-            connected = 1;                                         
-        } else if (state == ZOO_AUTH_FAILED_STATE) {               
-            zookeeper_close(zzh);                                  
-            exit(1);                                               
-        } else if (state == ZOO_EXPIRED_SESSION_STATE) {           
-            zookeeper_close(zzh);                                  
-            exit(1);                                               
-        }                                                          
 
-    }   
-    printf("init watcher called! \n");
+void get_shared_state(){
+    printf("get shared state, implement me\n");
 }
-c
 
-void create_ephemeral_sequential_node(zhandle_t *zzh){
-	printf("creating node, please implement me\n");
-
-	static char value[] = "localhost:9200";
-
-	int rc = zoo_acreate(zzh, master_znode_base_path, value, 14,
-    &ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL | ZOO_SEQUENCE, NULL, NULL);
-
-    if(rc = ZOK){
-    	printf("creating a node with my data \n");
+void zktest_string_completion (int RC, const  char * name, const  void * Data)
+{
+    if (! RC) {
+        fprintf (stderr, "znode created =%s  " , name);
     }
 }
 
+void create_ephemeral_sequential_node(char *value){
+    printf("creating node, please implement me\n");
+
+    int rc = zoo_acreate(zh, master_znode_base_path, value, 14,
+    &ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL | ZOO_SEQUENCE, 
+    zktest_string_completion, NULL);
+
+    if(rc = ZOK){
+        printf("creating a node with my data \n");
+    }else{
+        fprintf(stderr, "Error in creating znode %d\n", rc);  
+         return;
+    }
+}
+
+void initZooHandle(){
+
+    zh = zookeeper_init("localhost:2181", NULL, 30000, 0, 0, 0);
+    if(!zh){
+        printf("error");
+        return;
+    }
+    fprintf(stderr, "Opened zookeeper\n");  
+}
+
+void deleteZooHandle(){
+    zookeeper_close(zh);
+}
+
+
 //create -s -e /master/dmtcp_coord_ "localhost:1119"
 int main(int argc, char **argv){
-	printf("hello zookeeper\n");
+    printf("hello zookeeper\n");
+    static char value[] = "localhost:9219";
 
-	char buffer[512];
-	zh = zookeeper_init("localhost:2181", watcher, 30000, 0, 0, 0);
+    initZooHandle();
 
-	if(!zh){
-		printf("error");
-	}
+    create_ephemeral_sequential_node(value);
+   
+    get_shared_state();
 
-	int buflen = sizeof(buffer);
-	struct Stat stat;
-	
-
-    create_ephemeral_sequential_node(zh);
-
-    
     printf("sleeping! \n");
     // do whatever master does
     while(1) sleep(1);  
 
-
-	fprintf(stderr, "Closing zookeeper\n");  
-	
-	zookeeper_close(zh);
-
-
-	return 0;
+    fprintf(stderr, "Closing zookeeper\n");  
+    
+    deleteZooHandle();
+    return 0;
 }
--- zookeeper/queryMasterCoord.c
+++ zookeeper/queryMasterCoord.c
@@ -9,7 +9,7 @@ static int connected;
 char gpath[1024];  
 static char mycontext[] = "testingdata";
 char buffer[255]; 
-int master_number = 999;	
+int master_number = 999;    
 char master_string[23];
 struct String_vector vector; 
 char master_znode_base_path[] = "/master";
@@ -24,7 +24,7 @@ void watcher(zhandle_t *zzh, int type, int state, const char *path,
              void* context)                                        
 {                                                                  
     if (type == ZOO_SESSION_EVENT) {   
-    	printf("zoo event changed : watcher \n");                            
+        printf("zoo event changed : watcher \n");                            
         if (state == ZOO_CONNECTED_STATE) {                        
             connected = 1;                                         
         } else if (state == ZOO_AUTH_FAILED_STATE) {               
@@ -41,50 +41,51 @@ void watcher(zhandle_t *zzh, int type, int state, const char *path,
 
 
 void leaderFinding(zhandle_t *zh){
-	
+    
     int rc= zoo_get_children(zh, master_znode_base_path, 1, &vector);  
     if(rc != ZOK) { 
      fprintf(stderr,"electLeader: get_children failed\n");  
     } else { 
-	   	int i=0;  
-	   	fprintf(stderr, "checking for coordinators which are available \n");  
-	   	char other_master_string[11];
-	   	
-	   
-	   	int check_coord_number = -1;
-	    other_master_string[11] = '\0';
+        int i=0;  
+        fprintf(stderr, "checking for coordinators which are available \n");  
+        char other_master_string[11];
+        
+       
+        int check_coord_number = -1;
+        other_master_string[11] = '\0';
 
-		for(i=0;i<vector.count;i++){  
-	        fprintf(stderr, "%s\n", vector.data[i]);
-	        
-	        strncpy(other_master_string, vector.data[i] + 12, 10);  
-	        printf("checking other_master node : %s \n", other_master_string);
+        for(i=0;i<vector.count;i++){  
+            fprintf(stderr, "%s\n", vector.data[i]);
+            
+            strncpy(other_master_string, vector.data[i] + 12, 10);  
+            printf("checking other_master node : %s \n", other_master_string);
 
-	        // dmtcp_coord_ from dmtcp_coord_0000000001
-	        check_coord_number = atoi(other_master_string);
-	        if(master_number > check_coord_number){
-	        	printf("\tupdating master coordinator as %d \n", check_coord_number);
-	        	master_number = check_coord_number;
-	        	strncpy(master_string, vector.data[i], 23);
-	        }
-	        printf("\n");	        
-	    }
-	}
+            // dmtcp_coord_ from dmtcp_coord_0000000001
+            check_coord_number = atoi(other_master_string);
+            if(master_number > check_coord_number){
+                printf("\tupdating master coordinator as %d \n", check_coord_number);
+                master_number = check_coord_number;
+                strncpy(master_string, vector.data[i], 23);
+            }
+            printf("\n");           
+        }
+    }
 
-	fprintf(stderr, "Master determined from znode cluster is  %d with %s \n", master_number, master_string);
-	int len = 254; 
-	struct Stat st; 
-	char full_master_path[30];
-	strcpy(full_master_path, full_master_znode_base_path);
-	
-	strcat(full_master_path, master_string);
+    fprintf(stderr, "Master determined from znode cluster is  %d with %s \n", master_number, master_string);
+    int len = 254; 
+    struct Stat st; 
+    char full_master_path[30];
+    strcpy(full_master_path, full_master_znode_base_path);
+    
+    strcat(full_master_path, master_string);
 
-	rc = zoo_wget(zh, full_master_path, watcherforwget, mycontext, buffer, &len, &st);
+    rc = zoo_wget(zh, full_master_path, watcherforwget, mycontext, buffer, &len, &st);
 
-	if(rc != ZOK) {  
-     	printf("Problems %s %d\n", full_master_path, rc); 
+    if(rc != ZOK) {  
+        printf("Problems %s %d\n", full_master_path, rc); 
     } else { 
-    	printf("Data: %s\n", buffer);
+        printf("Data: %s\n", buffer);
+        // break here
     }
 }
 
@@ -116,11 +117,12 @@ void watcherforwget(zhandle_t *zzh, int type, int state, const char *path,
            printf("Path: %s changed data: %s\n", path, buffer);                
         }                                                                      
     } else if (type == ZOO_DELETED_EVENT ){
-    	printf("some node was deleted, lets elect leader again! \n");
-    	// call leader election here
-    	// before election remove the cached master with us
-    	master_number = 999;
-    	leaderFinding(zzh);
+        printf("some node was deleted, lets elect leader again! \n");
+        
+        // before election remove the cached master with us
+        master_number = 999;
+        // calling leader finding algorithm again
+        leaderFinding(zzh);
     }                                                                         
  
     printf("Watcher context %s\n", p);
@@ -129,18 +131,18 @@ void watcherforwget(zhandle_t *zzh, int type, int state, const char *path,
 
 
 int main(int argc, char **argv){
-	printf("hello zookeeper\n");
+    printf("hello zookeeper\n");
 
-	char buffer[512];
-	zh = zookeeper_init("localhost:2181", watcher, 30000, 0, 0, 0);
+    char buffer[512];
+    zh = zookeeper_init("localhost:2181", watcher, 30000, 0, 0, 0);
 
-	if(!zh){
-		printf("error");
-	}
+    if(!zh){
+        printf("error");
+    }
 
-	int buflen = sizeof(buffer);
-	struct Stat stat;
-	
+    int buflen = sizeof(buffer);
+    struct Stat stat;
+    
 
     leaderFinding(zh);
 
@@ -150,12 +152,12 @@ int main(int argc, char **argv){
     while(1);  
 
 
-	fprintf(stderr, "Closing zookeeper\n");  
-	
-	zookeeper_close(zh);
+    fprintf(stderr, "Closing zookeeper\n");  
+    
+    zookeeper_close(zh);
 
 
-	return 0;
+    return 0;
 }
 
 
